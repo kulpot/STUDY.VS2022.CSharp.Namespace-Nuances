ea778814bb35fd4642642b525dde67e613593afc
Kulpot <kulpotwahu@gmail.com>
2023-09-13 22:58:35 +0800

    Add project files.

---
+++ Namespace Nuances/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Kulpot;
+using Dog = Kulpot.Animals.FurryFriends.longgggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg;
+//using Kulpot.Animals.FurryFriends;
+//using Your;   // explicit must .Dog
+
+//ref link:https://www.youtube.com/watch?v=R6xzWVAijjI&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=54
+//
+
+namespace Kulpot.Animals.FurryFriends
+{
+    //class Dog { }
+    class longgggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg { }
+}
+
+//namespace Kulpot
+//{
+//    //namespace Animals
+//    //{
+//    //    namespace FurryFriends
+//    //    {
+//    //        class Dog { }
+//    //    }
+//    //}
+//}
+
+//namespace Your
+//{
+//    class Dog { }
+//}
+
+class MainClass
+{
+    static void Main()
+    {
+        Dog dog = new Dog();
+        //Kulpot.Animals.FurryFriends.Dog dog = new Dog();
+        //Kulpot.Animals.FurryFriends.Dog dog = new Kulpot.Animals.FurryFriends.Dog();
+
+
+        //Kulpot.Dog dog = new Kulpot.Dog();
+        //Your.Dog yDog = new Your.Dog();
+        //Dog usingKulpotdog = new Dog();
+    }
+}
